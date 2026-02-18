9e6042e34999915ed3e7cb91806ddf427b3e131b
xcillik <jakub@cillik.org>
2026-02-18 11:00:04 +0100

    Setup showcase app migration to Next.js

--- postcss.config.ts
+++
@@ -1,9 +0,0 @@
-// Source - https://stackoverflow.com/a
-// Posted by flydev, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-01-20, License - CC BY-SA 4.0
-
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
-};
