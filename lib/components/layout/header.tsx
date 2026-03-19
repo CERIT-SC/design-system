c02882ad945fd074a3c6e2fb42299917e25ddf1a
Romana Ďuráčiová <98467853+romanaduraciova@users.noreply.github.com>
2026-03-19 15:16:21 +0100

    feat: add sidebar component to docs layout

--- lib/components/layout/header.tsx
+++ lib/components/layout/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { cn } from "../../lib/utils";
 import { Button } from "../primitives/button";
-import { SidebarTrigger } from "./sidebar";
 import { Separator } from "../primitives/separator";
 import {
   NavigationMenu,
