27fb93765fede69474f079a90cf94f293364ee10
Romana Ďuráčiová <98467853+romanaduraciova@users.noreply.github.com>
2026-03-26 12:08:25 +0100

    feat: add storybook docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
   stories: ["../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [],
+  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
